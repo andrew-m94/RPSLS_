15b5943daf9eece1cad18ad8d0592ab4fa9c6069
RaHughes <rogerallenhughes@hotmail.com>
2021-10-15 15:50:26 -0500

    Add functionality to play_round and compare_gesture method

--- play_area.py
+++ play_area.py
@@ -1,6 +1,7 @@
 from player import Player
 from human import Human
 from robot import Robot
+import random
 
 class PlayArea:
     def __init__(self):
@@ -21,7 +22,21 @@ class PlayArea:
         self.players.append(player_two)
 
     def welcome_message(self):
-        pass
+        print('Welcome')
+
+    def play_round(self):
+        print('Please input the number of the Gesture you would like.')
+        print('1.) Rock')
+        print('2.) Paper')
+        print('3.) Scissors')
+        print('4.) Lizard')
+        print('5.) Spock')
+        user_input1 = int(input(f'{self.players[0].name} Insert number here: '))
+        self.players[0].pick_gesture(user_input1)
+        user_input2 = int(input(f'{self.players[1].name} Insert number here'))
+        self.players[1].pick_gesture(user_input2)
+        self.compare_gesture(self.players[0].current_pick, self.players[1].current_pick)
+        
 
     def choose_game_mode(self):
         user_input = input('Would you like to play Singleplayer or Multiplayer: ').lower()
@@ -38,9 +53,20 @@ class PlayArea:
                            [1, -1, 1, -1, 0]]
 
         winner = compare_gesture[p1_pick][p2_pick] 
+        if winner == -1:
+            print(f'{self.players[1].name} has won this round!')
+            self.players[1].score += 1
+        elif winner == 0:
+            print('This round is a tie!')
+        elif winner == 1:
+            print(f'{self.players[0].name} has won this round!')
+            self.players[0].score += 1        
 
     def display_winner(self):
         pass
 
     def run_game(self):
-        pass
+        self.welcome_message()
+        self.choose_game_mode()
+        self.play_round()
+  
