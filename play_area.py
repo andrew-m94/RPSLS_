e9a95bd4bebf2ae96690e7c0e23e8d85b746f47a
RaHughes <rogerallenhughes@hotmail.com>
2021-10-15 14:47:35 -0500

    Add functionality to some play_area methods

--- play_area.py
+++ play_area.py
@@ -4,18 +4,31 @@ from robot import Robot
 
 class PlayArea:
     def __init__(self):
-        self.gesture_list = []
+        self.gesture_list = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']
         self.max_score = 2
         self.players = []
 
     def add_players(self):
-        pass
+        player_one = Human()
+        player_two = Human()
+        self.players.append(player_one)
+        self.players.append(player_two)
+
+    def add_player_and_ai(self):
+        player_one = Human()
+        player_two = Robot()
+        self.players.append(player_one)
+        self.players.append(player_two)
 
     def welcome_message(self):
         pass
 
     def choose_game_mode(self):
-        pass
+        user_input = input('Would you like to play Singleplayer or Multiplayer: ').lower()
+        if user_input == 'singleplayer':
+            self.add_player_and_ai()
+        elif user_input == 'multiplayer':
+            self.add_players()    
 
     def display_winner(self):
         pass
