6a62c84a75857e04663910144da6872ff39c5b74
RaHughes <rogerallenhughes@hotmail.com>
2021-10-15 14:47:05 -0500

    Made name method internal to human class

--- human.py
+++ human.py
@@ -1,6 +1,10 @@
 from player import Player
 
 class Human(Player):
-    def __init__(self, name):
-        self.name = name
+    def __init__(self):
+        self.name = self.get_name()
         super().__init__(self)
+
+    def get_name(self):
+        user_input = input('Please insert your name here: ')
+        self.name = user_input
